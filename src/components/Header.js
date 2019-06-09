d24819bea0dc67fdf2b474bb607fe8ba215591fa
Mikal Morello <mmorel01@dhcp-130-64-12-165.medford.tufts.edu>
2019-06-09 18:25:41 -0400

    JS - Add Sticky Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,40 @@ import {
 } from 'react-router-dom';
 
 class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      y: 0
+    };
+  }
+  
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll, { passive: true })
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll(event) {
+    var header = document.getElementById('header'),
+      headerHeight = header.clientHeight,
+      scrollAmount = window.scrollY,
+      windowHeight = window.innerHeight,
+      headerOffset = windowHeight; 
+
+    if (scrollAmount >= headerHeight) {
+      header.classList.add('header--scroll');
+    } else {
+      header.classList.remove('header--scroll');
+    }
+
+    if (scrollAmount > headerOffset) {
+      header.classList.add("header--sticky");
+    } else {
+      header.classList.remove("header--sticky");
+    }
+  }
   
   checkMenuState(menuState){
     if (menuState === 'true') {
@@ -28,7 +62,10 @@ class Header extends React.Component {
 
   render(){
     return (
-      <header id="header" className={`header ${this.setHeaderColor(this.props.menuState, this.props.headerStyle)}`}>
+      <header
+        id="header" 
+        className={`header ${this.setHeaderColor(this.props.menuState, this.props.headerStyle)}`}
+      >
         <div className="header__container">
           <div className="header__brand">
             <Link to="/" className="header__logo">
